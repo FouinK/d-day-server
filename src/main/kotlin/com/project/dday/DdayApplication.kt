ec7bf10d139ca54a940ebbfa54ae61a411f95a7c
Roy <yyss9103@gmail.com>
2023-12-04 22:10:00 +0900

    first commit

---
+++ src/main/kotlin/com/project/dday/DdayApplication.kt
@@ -0,0 +1,11 @@
+package com.project.dday
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DdayApplication
+
+fun main(args: Array<String>) {
+	runApplication<DdayApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/dday/DdayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.dday
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DdayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
