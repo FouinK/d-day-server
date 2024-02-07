8161f8cec419b6ff1e24bd37083da6ec32bb5a80
Roy <yyss9103@gmail.com>
2024-02-07 23:10:00 +0900

    ㅌㅔ스트 환경 구성 완료

--- src/test/kotlin/com/project/dday/application/ask/action/PostAskActionTest.kt
+++ src/test/kotlin/com/project/dday/application/ask/action/PostAskActionTest.kt
@@ -1,26 +1,17 @@
-package com.project.dday.action.answer
+package com.project.dday.application.ask.action
 
-import com.project.dday.application.ask.action.PostAskAction
 import com.project.dday.application.ask.port.`in`.PostAskUseCase
-import com.project.dday.repository.AnswerRepository
 import com.project.dday.repository.AskRepository
-import com.project.dday.service.AskService
-import com.project.dday.service.CoupleService
 import com.project.dday.service.MemberService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
-@ActiveProfiles("test")
 @SpringBootTest
-class PostAnswerActionTest(
+class PostAskActionTest(
     @Autowired val memberService: MemberService,
-    @Autowired val askService: AskService,
     @Autowired val askRepository: AskRepository,
-    @Autowired val coupleService: CoupleService,
-    @Autowired val answerRepository: AnswerRepository,
 ) {
     lateinit var postAskUseCase: PostAskUseCase
 
@@ -31,7 +22,9 @@ class PostAnswerActionTest(
             askRepository,
         )
 
-//        postAskUseCase.ask()
+//        postAskUseCase.ask(
+//
+//        )
     }
 
     @Test
